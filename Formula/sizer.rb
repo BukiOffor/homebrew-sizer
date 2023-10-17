a4bb0115ce7feb056b04de74b9cc228b9a0302a7
Ebuka <ebuka2264@yahoo.com>
2023-10-18 00:15:16 +0100

    tap: created

--- Formula/sizer.rb
+++ Formula/sizer.rb
@@ -1,8 +1,8 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-class Espanso < Formula
-    desc "Cross-platform Text Expander written in Rust"
+class Sizer < Formula
+    desc "A file sizing tool written in Rust"
     homepage "https://github.com/BukiOffor/file-sizer"
     url "https://github.com/BukiOffor/file-sizer/releases/download/v0.1.0/sizer-mac.tar.gz"
     sha256 "6f669951ee29504559273535cf93bbeec046942da98c57b3d07ed0aa57e474ab"
